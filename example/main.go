bcf92832f1a8f8a275c97238089c544eb6ce8cd5
epitchi <thienvanlea1@gmail.com>
2024-03-15 20:55:31 +0700

    customer ui

--- example/main.go
+++ example/main.go
@@ -13,26 +13,31 @@ func main() {
 		panic(err)
 	}
 
+	fmt.Println("Logged in as")
+
 	now := time.Now()
 	err = client.SetActivity(client.Activity{
-		State:      "Heyy!!!",
-		Details:    "I'm running on rich-go :)",
-		LargeImage: "largeimageid",
-		LargeText:  "This is the large image :D",
-		SmallImage: "smallimageid",
-		SmallText:  "And this is the small image",
-		Party: &client.Party{
-			ID:         "-1",
-			Players:    15,
-			MaxPlayers: 24,
-		},
+		State:      "grupobright.com",
+		Details:    "Playing in Bright Cloud",
+		// LargeImage: "./bcg_branco.png",
+		// LargeText:  "This is the large image :D",
+		// SmallImage: "smallimageid",
+		// SmallText:  "And this is the small image",
+		LargeImage: "bcg_branco",
+		SmallImage: "bcg_branco",
+
+		// Party: &client.Party{
+		// 	ID:         "-1",
+		// 	Players:    15,
+		// 	MaxPlayers: 24,
+		// },
 		Timestamps: &client.Timestamps{
 			Start: &now,
 		},
 		Buttons: []*client.Button{
 			&client.Button{
-				Label: "GitHub",
-				Url:   "https://github.com/hugolgst/rich-go",
+				Label: "Discord",
+				Url:   "https://discord.gg/grupobright",
 			},
 		},
 	})
@@ -44,5 +49,5 @@ func main() {
 	// Discord will only show the presence if the app is running
 	// Sleep for a few seconds to see the update
 	fmt.Println("Sleeping...")
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 59999)
 }
